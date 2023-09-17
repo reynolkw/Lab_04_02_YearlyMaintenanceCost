711e7fd42d821cbc2c7b6e94b90a7964685ac8c1
hueforge <111766391+reynolkw@users.noreply.github.com>
2023-09-16 23:00:46 -0400

    added functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // simulating user input
+        double winterCost = 400.00;
+        double springCost = 300.00;
+        double summerCost = 600.00;
+        double fallCost = 300.00;
+
+        double yearlyCost = 0.0;
+        yearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("Winter cost: $" + winterCost);
+        System.out.println("Spring cost: $" + springCost);
+        System.out.println("Summer cost: $" + summerCost);
+        System.out.println("Fall cost: $" + fallCost);
+        System.out.println("The total yearly maintenance cost is $" + yearlyCost);
     }
 }
